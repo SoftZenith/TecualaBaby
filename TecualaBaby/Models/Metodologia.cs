fd0ef0a83f8e21666e2a0472da392dca19c259fb
Brayan Jesús Gutiérrez Esparza <bhn_rock@hotmail.com>
2018-04-18 21:24:18 -0600

    Models

---
+++ TecualaBaby/Models/Metodologia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TecualaBaby.Models
+{
+    public class Metodologia
+    {
+        public int clave { get; set; }
+        public string descripcion { get; set; }
+        public string explicacion { get; set; }
+    }
+}
