5ca7934876ff615f48a74f1e2bb3f9530735fc5c
Brayan Jesús Gutiérrez Esparza <bhn_rock@hotmail.com>
2018-04-18 21:34:13 -0600

    DbContext

---
+++ TecualaBaby/Models/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TecualaBaby.Models
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Metodologia> Metodologias { get; set; }
+    }
+}
